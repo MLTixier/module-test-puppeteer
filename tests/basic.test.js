a5da44ce7ad2abe24af25f96a3c6fa0b6f9fe244
Marie-Laure TIXIER <marie-laure.tixier@ponant-technologies.com>
2023-01-11 14:19:39 +0100

    fix url https and update expected contents

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -7,19 +7,19 @@ describe("Tests basiques", () => {
     // vérification du chargement de la page d'accueil
     test('home', async () => {
         // charger la page d'accueil
-        await page.goto('http://polr.stationmyr.net');
+        await page.goto('https://polr.stationmyr.net/');
         // attendre que l'élément <body> soit chargé
         await page.waitForSelector('body');
         // récupérer le contenu de l'élément <body>
         const html = await page.$eval('body', e => e.innerHTML);
         // vérifier que dans cet élément Body on trouve "Polr du campus"
         await page.screenshot({path: './tests/img/basic-home.png'});
-        expect(html).toContain("Polr - Campus Valence")
-    }, timeout);
+        expect(html).toContain("Shorturl")
+    }, timeout);git add .
 
     // parcours client avec about
     test('home and about', async () => {
-        await page.goto('http://polr.stationmyr.net');
+        await page.goto('https://polr.stationmyr.net');
         await page.waitForSelector('#navbar li a');
         // click sur le lien "About" de la navigation
         await page.evaluate( () => {
